9e82260cb8ad468fa0b457bf4ee3c47675686f08
Nicola Staniscia <66362350+NicolaStaniscia@users.noreply.github.com>
2024-11-25 17:08:58 +0100

    Delete consegna directory

--- consegna/src/market/trading_history/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("MARKET_DB_URI")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DBM_URL = os.getenv("DBM_URL", "https://db_manager:5000/db_manager/")
-    JWT_SECRET_KEY = os.getenv("SECRET_KEY", "JwtGACHA2425")
